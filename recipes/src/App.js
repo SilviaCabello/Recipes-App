5e6b3cafdb9fbaab85ce36ee8f21698b2517cb64
SilviaCabello <silviacabellom@gmail.com>
2021-05-19 11:13:45 +0200

    add form

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -5,11 +5,14 @@ function App() {
   const APP_ID = "d6c939d9";
   const APP_KEY = "b52ecc7bf4d89f472291cd4518360a3a";
 
-  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form>
+        <input type="text"/>
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
