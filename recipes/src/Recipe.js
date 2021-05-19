b93bc5b7cfcbcbd24255a1bafa8f564b70131f16
SilviaCabello <silviacabellom@gmail.com>
2021-05-19 16:11:43 +0200

    create a module.css for recipe.js

--- recipes/src/Recipe.js
+++ recipes/src/Recipe.js
@@ -1,6 +1,8 @@
+import style from "./recipe.module.css";
+
 const Recipe = ({ title, image, calories }) => {
   return (
-    <div>
+    <div className={style.recipe}>
       <h3>{title}</h3>
       <p>Calories: {calories}</p>
       <img src={image} alt="" />
